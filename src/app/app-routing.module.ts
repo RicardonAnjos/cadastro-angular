37bb808164f15371de1e21ae064e7a045a969967
Ricardo Anjos <ricardoanj.14@gmail.com>
2023-03-16 20:50:45 -0300

    setup inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroClienteComponent } from './clientes/components/cadastro-cliente/cadastro-cliente.component';
+import { ListaClienteComponent } from './clientes/components/lista-cliente/lista-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListaClienteComponent},
+  { path: 'clientes', component: ListaClienteComponent },
+  { path: 'cadastrar', component: CadastroClienteComponent },
+  { path: 'clientes/:id', component: CadastroClienteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaClienteComponent } from './clientes/components/lista-cliente/lista-cliente.component';
+import { CadastroClienteComponent } from './clientes/components/cadastro-cliente/cadastro-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaClienteComponent,
+    CadastroClienteComponent
   ],
   imports: [
     BrowserModule,
