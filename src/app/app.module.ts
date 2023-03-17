c6d3c282bf8d7943cb645950f00a93e43f2719cd
Ricardo Anjos <ricardoanj.14@gmail.com>
2023-03-16 21:01:19 -0300

    implementa um navbar parsial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaClienteComponent } from './clientes/components/lista-cliente/lista-cliente.component';
 import { CadastroClienteComponent } from './clientes/components/cadastro-cliente/cadastro-cliente.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaClienteComponent,
-    CadastroClienteComponent
+    CadastroClienteComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
